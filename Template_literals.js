fed6429bff30c9267e72471e7f32eaf4384c508a
AmanRaj0904 <aman1462001@gmail.com>
2023-12-24 14:23:00 +0530

    Template Literals

---
+++ Template_literals.js
@@ -0,0 +1,19 @@
+let str = `my
+name
+is       
+               aman
+               
+               
+               raj`;
+
+console.log(str); // This will print same as written on variable
+
+// want to conactenate through template literals
+
+let str1 = "AMAN";
+console.log(`My name is ${str1}.`); // here string concatenated.
+
+// we can also evaluate the expression inside ${}
+
+let num=2;
+console.log(`4 into ${num} = ${4*2} `);
